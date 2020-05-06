cb090ce9954f2440b6ab9ad44ae73f112bac2837
Whashi <whashi88@gmail.com>
2020-05-05 19:04:34 -0700

    finished the attributes of the class and need to put in the arithmatic

--- MainPage.js
+++ MainPage.js
@@ -1,17 +1,43 @@
-let hourlyPayRate = 20;
-let hoursWorkedPerWeek = 40;
-// let overTime = 40 + ((hoursWorkedPerWeek - 40) * 1.5);
-let payFrequency = 52 / 26;
-let grossIncome = hoursWorkedPerWeek * hourlyPayRate * payFrequency;
-let grossYearlyIncome = hoursWorkedPerWeek * hourlyPayRate * 52;
+let jRizz = {
+  hourlyPayRate : 20,
+  hoursWorkedPerWeek : 40,
+  //  overTime : 40 + ((hoursWorkedPerWeek - 40) * 1.5),
+  payFrequency : 52 / 26,
+  grossIncome : this.hoursWorkedPerWeek * this.hourlyPayRate * this.payFrequency,
+  grossYearlyIncome : this.hoursWorkedPerWeek * this.hourlyPayRate * 52,
 
-let medicalPerMonth = 365.75;
-let visionPerMonth = 27.6;
-let dentalPerMonth = 118.85;
-let monthlyBenefitsExpense = medicalPerMonth + visionPerMonth + dentalPerMonth;
-let paycheckDeduction = monthlyBenefitsExpense / payFrequency;
+  medicalPerMonth : 365.75,
+  visionPerMonth : 27.6,
+  dentalPerMonth : 118.85,
+  monthlyBenefitsExpense : this.medicalPerMonth + this.visionPerMonth + this.dentalPerMonth,
+  paycheckDeduction : this.monthlyBenefitsExpense / this.payFrequency,
 
-let stateTaxWithholding = 0.036;
-// fed
-let socialSecurityWithholding = .062;
-let medicareWithholding = .0145;
+  stateTaxWithholding : 0.036,
+  // fed
+  socialSecurityWithholding : .062,
+  medicareWithholding : .0145,
+
+  electricity : 100,
+  water : 45,
+  trash : 15,
+  gas : 25,
+  phones : 120,
+  rent : 840,
+  internet : 80,
+  netflix : 12,
+  braces : 0,
+  hospital : 0,
+  spotify : 0,
+  fuel : 80,
+  xbox : 0,
+  insurance : 223,
+  malibu : 334,
+  diapers : 100,
+};
+
+let p = document.creatElement("p");
+let newNode = document.createTextNode(jRizz.electricity);
+p.appendChild(node);
+let div = document.getElementById("demo");
+div.appendChild(p);
+// console.log(jRizz.electricity);
